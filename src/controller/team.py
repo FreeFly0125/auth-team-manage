885ae670067cd5994c7357b95901738a0eca585d
FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>
2024-10-07 22:55:25 +0200

    feat: impl change user role

--- src/controller/team.py
+++ src/controller/team.py
@@ -8,10 +8,13 @@ def get_team_with_id(id):
     return Team.objects.get(id=id)
 
 
-def remove_user_from_team(user_id):
-    Team.objects(members__user_id=user_id).update(pull__members__user_id=user_id)
+def remove_user_from_team(team_id, user_id):
+    Team.objects(id=team_id, members__user_id=user_id).update(pull__members__user_id=user_id)
 
 
 def is_user_team_member(team_id, user_id):
     member = Team.objects(id=team_id, members__user_id=user_id).first()
     return member is not None
+
+def update_user_role(team_id, user_id, role):
+    Team.objects(id=team_id, members__user_id=user_id).update_one(set__members__S__role=role)
--- src/routes/team/route.py
+++ src/routes/team/route.py
@@ -99,7 +99,29 @@ def delete_team_member():
     if not team_controller.is_user_team_member(g.payload["teamID"], g.payload["userID"]):
         raise UserNotExistError()
     
-    team_controller.remove_user_from_team(g.payload["userID"])
+    team_controller.remove_user_from_team(g.payload["teamID"], g.payload["userID"])
+    return response(success=True)
+
+@team.route("/change_role", methods=["PATCH"])
+@validate("change_user_role")
+def change_user_role():
+    if not team_controller.is_user_team_member(g.payload["teamID"], g.payload["userID"]):
+        raise UserNotExistError()
+    
+    t = team_controller.get_team_with_id(g.payload["teamID"])
+    if g.payload["role"] == "user":
+        if g.payload["userID"] not in t.admin:
+            raise UserAlreadySameRoleError()
+        
+        team_controller.update_user_role(g.payload["teamID"], g.payload["userID"], "user")
+        t.admin.remove(g.payload["userID"])
+    elif g.payload["role"] == "admin":
+        if g.payload["userID"] in t.admin:
+            raise UserAlreadySameRoleError()
+        
+        team_controller.update_user_role(g.payload["teamID"], g.payload["userID"], "admin")
+        t.admin.append(g.payload["userID"])
+    t.save()
     return response(success=True)
 
 
@@ -125,3 +147,7 @@ class UserExistInTeamError(APIException):
 class UserNotExistError(APIException):
     def __init__(self):
         super(UserNotExistError, self).__init__(status_code=404, error_code=1207, message="The user is not found within the team.")
+
+class UserAlreadySameRoleError(APIException):
+    def __init__(self):
+        super(UserAlreadySameRoleError, self).__init__(status_code=409, error_code=1208, message="The user is already set as the role.")
